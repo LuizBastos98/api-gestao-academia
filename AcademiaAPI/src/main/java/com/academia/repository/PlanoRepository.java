4d04daf384118c0df0f4a2bd4f5638efc133010d
LuizBastos98 <162064092+LuizBastos98@users.noreply.github.com>
2025-10-06 18:29:24 -0300

    Update PlanoRepository.java

--- AcademiaAPI/src/main/java/com/academia/repository/PlanoRepository.java
+++ AcademiaAPI/src/main/java/com/academia/repository/PlanoRepository.java
@@ -1,7 +1,8 @@
-// src/main/java/com/academia/repository/PlanoRepository.java
+
 package com.academia.repository;
 
 import com.academia.model.Plano;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PlanoRepository extends JpaRepository<Plano, Long> {}
+
