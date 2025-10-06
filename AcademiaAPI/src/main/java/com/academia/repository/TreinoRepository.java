05c8acb107b4a2a23fd25fcd9e21b8470fa6023a
LuizBastos98 <162064092+LuizBastos98@users.noreply.github.com>
2025-10-06 18:29:34 -0300

    Update TreinoRepository.java

--- AcademiaAPI/src/main/java/com/academia/repository/TreinoRepository.java
+++ AcademiaAPI/src/main/java/com/academia/repository/TreinoRepository.java
@@ -1,7 +1,8 @@
-// src/main/java/com/academia/repository/TreinoRepository.java
+
 package com.academia.repository;
 
 import com.academia.model.Treino;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TreinoRepository extends JpaRepository<Treino, Long> {}
+
